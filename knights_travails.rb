89a5bf8e5f626cc7185f311ac9dfe4340b823f8e
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-06 21:57:36 +0530

    Implemented the search algorithm

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 TRANSFORMATIONS = [
   [+ 2,  + 1],
   [+ 2,  - 1],
@@ -22,6 +24,35 @@ def valid_move?(move)
   (move[0]).between?(0, 7) && (move[1]).between?(0, 7)
 end
 
-def knight_moves(start, stop)
-  puts start, stop
+def find_end(start, stop)
+  queue = [Node.new(start, nil)]
+  cur_node = queue.shift
+  until cur_node.square == stop
+    possible_moves(cur_node.square).each do |move|
+      new_node = Node.new(move, cur_node)
+      queue << new_node
+      cur_node.neighbours << new_node
+    end
+    cur_node = queue.shift
+  end
+  cur_node
 end
+
+def build_path(stop, start)
+  path = [stop]
+  until stop.square == start
+    stop = stop.parent
+    path << stop
+  end
+  path
+end
+
+def knight_moves(start, stop)
+  stop_location = find_end(start, stop)
+  build_path(stop_location, start).reverse
+end
+
+start = [3, 3]
+stop = [4, 3]
+p = knight_moves(start, stop)
+puts p
